ce436c01c81a4f0aa5a5d83efc622d2214a6083a
lynn84961 <lyuting.wu@gmail.com>
2023-12-09 22:14:42 -0800

    Fix underline offline animation

--- src/component/navigation/NavigationButton.tsx
+++ src/component/navigation/NavigationButton.tsx
@@ -8,8 +8,8 @@ export const NavigationButton : React.FC<NavigationBtnProps> = ({buttonText} ) =
   return (
     <div className={`
       text-green text-lg font-title font-medium 
-        self-center mx-2 h-full px-2 pt-1
-      hover:text-pink hover:decoration-solid hover:underline hover:underline-offset-[6px] duration-700`}>
+        self-center mx-2 h-full px-2 pt-1 underline-offset-[6px]
+      hover:text-pink hover:decoration-solid hover:underline duration-700`}>
         {buttonText}
     </div>
   )
