2d0cd976a5cad404a98a2479cc3d5cc556f757e9
lynn84961 <lyuting.wu@gmail.com>
2023-12-10 02:09:23 -0800

    Add a build in progress text

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="h-screen w-screen bg-[#272727]">
       <NavigationBar/>
+      <div className="mt-12 w-fit text-transparent bg-clip-text self-center mx-auto font-title bg-gradient-to-r from-pink from-[20%] to-green">
+        Thank you for visiting. Website build-in-progress...
+      </div>
     </div>
   )
 }
--- src/component/navigation/WebsiteTitle.tsx
+++ src/component/navigation/WebsiteTitle.tsx
@@ -4,8 +4,9 @@ import icon from "../../assets/icon.svg"
 export const WebsiteTitle : React.FC = () => {
   return (
       <div className={`
-          text-xl font-title text-transparent bg-clip-text bg-green flex opacity-10
-          hover:bg-gradient-to-r hover:from-pink hover:from-[20%] hover:to-green hover:opacity-100 duration-700
+          text-xl font-title text-transparent bg-clip-text flex
+          bg-gradient-to-r from-pink from-[20%] to-green
+          opacity-10 hover:opacity-100 duration-700
         `}>
         <img src={icon} alt="" className="w-8 mr-2"/>
           Lyu-Ting Wu
