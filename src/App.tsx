a8f5ab1d9793aad0afb56b379de89afea2385f91
lynn84961 <lyuting.wu@gmail.com>
2023-12-10 00:29:43 -0800

    Added responsive for nav buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { NavigationBar } from "./component/navigation/NavigationBar"
 function App() {
 
   return (
-    <div className="h-screen bg-[#272727]">
+    <div className="h-screen w-screen bg-[#272727]">
       <NavigationBar/>
     </div>
   )
--- src/component/navigation/NavigationBar.tsx
+++ src/component/navigation/NavigationBar.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+
+import { useState } from 'react';
+
 import {WebsiteTitle} from "./WebsiteTitle";
 import { NavigationButton } from "./NavigationButton";
+import burgurMenu from "../../assets/burgerMenu.svg"
 
 export const NavigationBar : React.FC = () => {
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+
     return (
         <div className="w-full flex content-center justify-between px-4 py-2">
             <WebsiteTitle/>
-            <div className="flex opacity-10 hover:opacity-100 duration-700">
+            <div className="sm:hidden">
+                <img src={burgurMenu} alt="" className="w-[28px] h-full self-center" onClick={()=>setIsMenuVisible(!isMenuVisible)}/>
+                {isMenuVisible && <div className="absolute top-12 right-2 grid justify-items-end" >
+                    <NavigationButton buttonText="About"/>
+                    <NavigationButton buttonText="Projects"/>
+                    <NavigationButton buttonText="Resume"/>
+                </div>}
+            </div>
+            <div className="hidden sm:flex opacity-10 hover:opacity-100 duration-700">
                 <NavigationButton buttonText="About"/>
                 <NavigationButton buttonText="Projects"/>
                 <NavigationButton buttonText="Resume"/>
