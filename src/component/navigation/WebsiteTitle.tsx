48ab4bab617140f7337f23ce79d37ebea3877ae3
lynn84961 <lyuting.wu@gmail.com>
2023-12-09 23:48:38 -0800

    Added fonticon and adjusted hover color

--- src/component/navigation/WebsiteTitle.tsx
+++ src/component/navigation/WebsiteTitle.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import icon from "../../assets/icon.svg"
 
 export const WebsiteTitle : React.FC = () => {
   return (
-    <div className={`
-        text-xl font-title text-transparent bg-clip-text bg-green opacity-10
-        hover:bg-gradient-to-r hover:from-pink hover:to-green hover:opacity-100 duration-700`}>
-        Lyu-Ting Wu
-    </div>
+      <div className={`
+          text-xl font-title text-transparent bg-clip-text bg-green flex opacity-10
+          hover:bg-gradient-to-r hover:from-pink hover:from-[20%] hover:to-green hover:opacity-100 duration-700
+        `}>
+        <img src={icon} alt="" className="w-8 mr-2"/>
+          Lyu-Ting Wu
+      </div>
   )
 }
