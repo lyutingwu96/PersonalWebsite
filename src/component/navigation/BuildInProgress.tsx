9b3764dc61592a89d975242b173eb7a126be6b41
lynn84961 <lyuting.wu@gmail.com>
2024-01-26 00:25:03 -0800

    Edit the placeholder page

--- src/component/navigation/BuildInProgress.tsx
+++ src/component/navigation/BuildInProgress.tsx
@@ -6,7 +6,7 @@ export const BuildInProgress: React.FC = () => {
   return (
     <div className="w-screen min-h-screen flex justify-center items-center animate-fadeIn">
         <div className="fixed mx-auto text-transparent bg-clip-text text-center self-center font-title bg-gradient-to-r from-pink from-[20%] to-green">
-            Thank you for visiting.<br/>Page build-in-progress.
+            Thank you for visiting.<br/>Page coming soon.
         </div>
     </div>
   );
