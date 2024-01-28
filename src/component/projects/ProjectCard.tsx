fb525b9b58362b6eae94206f752b176091646c6e
lynn84961 <lyuting.wu@gmail.com>
2024-01-28 00:53:13 -0800

    Adjusted small screen style

--- src/component/projects/ProjectCard.tsx
+++ src/component/projects/ProjectCard.tsx
@@ -14,14 +14,14 @@ export interface ProjectCardProps {
 
 export const ProjectCard: React.FC<ProjectCardProps> = ({bannerImg, hoverImg, projectTitle, projectDescription, tags, path}) => {
   return (
-    <Link to={path} className="group w-[280px] sm:w-[330px] h-[330px] sm:h-[400px] border-[1px] border-green rounded-[5px] relative  duration-700 hover:border-pink  overflow-hidden">
+    <Link to={path} className="group w-[280px] sm:w-[330px] h-[350px] sm:h-[400px] border-[1px] border-green rounded-[5px] relative  duration-700 hover:border-pink  overflow-hidden">
         
         <ProjectImage bannerImg={bannerImg} hoverImg={hoverImg} />
 
         <div className="w-full float-bottom px-2 py-1">
             <div className="font-title text-lg text-green group-hover:text-pink duration-700 mb-2">{projectTitle}</div>
             <div className="font-content text-md text-light font-light pb-2">{projectDescription}</div>
-            <div className="gap-[6px] justify-end absolute bottom-2 right-2 flex-wrap-reverse hidden sm:flex">
+            <div className="gap-[6px] justify-end absolute bottom-2 right-2 flex-wrap-reverse flex">
                 {tags.map((tag) => <Tag tag={tag}/> )}
             </div>
         </div>
